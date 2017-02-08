c93219619a494b182622541e499ea9dbec20986e
Hendra Bratanata <hendrabratanata88@gmail.com>
2017-02-08 21:03:15 +0700

    *

---
+++ src/main/java/com/dicoding/menirukanmu/Event.java
@@ -0,0 +1,12 @@
+public class Event {
+
+    private Boolean success;
+    private List<Data> data;
+    public Boolean getSuccess ()
+    {
+        return success;
+    }
+    public List<Data> getData(){
+        return data;
+    }
+}
